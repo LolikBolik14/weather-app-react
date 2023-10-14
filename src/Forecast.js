24f2ec5468a1eac60b683cbc2c5022b56c1c306b
LolikBolik14 <shlyak14@gmail.com>
2023-10-14 18:47:20 -0400

    Fixed return bag in Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -36,6 +36,8 @@ export default function Forecast(props) {
                     <ForecastData data={dailyForecast} />
                   </div>
                 );
+              } else {
+                return null;
               }
             })}
           </div>
